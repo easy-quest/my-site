2978c4c52e5e39855788f02b89beda88cf3d1bf1
Ivan Yastrebov <easy.quest@vk.com>
2022-02-21 12:49:03 +0300

    .config/

--- navigation.php
+++ navigation.php
@@ -1,14 +1,44 @@
 <?php
 
 return [
-    'Getting Started' => [
-        'url' => 'docs/getting-started',
+    'Welcome' => 'docs/welcome',
+    'Ruby' => [
         'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
-            'Navigation' => 'docs/navigation',
-            'Algolia DocSearch' => 'docs/algolia-docsearch',
-            'Custom 404 Page' => 'docs/custom-404-page',
-        ],
+            'Arrays' => 'docs/ruby/arrays',
+            'Iteration' => 'docs/ruby/iterations',
+        ]
     ],
-    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+    'R' => 'docs/r',
+    'C#' => 'docs/csharp',
+    'C++' => 'docs/c',
+    'Docker' => 'docs/docker',
+    'Git' => [
+        'children' => [
+            'Workflow' => 'docs/git/workflow',
+            'Repository' => 'docs/git/repository',
+            'Branch' => 'docs/git/branch',
+            'Stash' => 'docs/git/stash',
+        ]
+    ],
+    'Java' => 'docs/java',
+    'Javascript' => 'docs/javascript',
+    'PHP' => [
+        'children' => [
+            'DateTime' => 'docs/php/datetime',
+            'Tooling' => 'docs/php/tools',
+            'Arrays' => 'docs/php/arrays',
+            'File System' => 'docs/php/filesystem'
+        ]
+    ],
+    'Python' => 'docs/python',
+    'Rails' => 'docs/rails',
+    'React' => 'docs/react',
+    'Vue' => 'docs/vue',
+    'Vim' => 'docs/vim',
+    'Kubernetes' => 'docs/kubernetes',
+    'REST API' => 'docs/restapi',
+    'Bash' => 'docs/bash',
+    'CSS' => 'docs/css',
+    'SCSS' => 'docs/scss',
+    'GO' => 'docs/go',
 ];
